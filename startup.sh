056631e3cf7e175f28f1d529b6aaea594efe2ce2
Stephen Archer <s.archer@f5.com>
2024-02-08 10:37:21 +0000

    added sudo

--- startup.sh
+++ startup.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-curl https://agent.connect.nginx.com/nginx-agent/install | DATA_PLANE_KEY="yIyz0sNub05qE1NcV0Ti8b8b9B2tt5yPwC3XQloe4os=" sh -s -- -y
+sudo curl https://agent.connect.nginx.com/nginx-agent/install | DATA_PLANE_KEY="yIyz0sNub05qE1NcV0Ti8b8b9B2tt5yPwC3XQloe4os=" sh -s -- -y
