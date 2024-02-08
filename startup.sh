9909d0def624844c842c658ec703147f2462add8
Stephen Archer <s.archer@f5.com>
2024-02-08 10:21:37 +0000

    made startup scrip executable

--- startup.sh
+++ startup.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-sudo curl https://agent.connect.nginx.com/nginx-agent/install | DATA_PLANE_KEY="yIyz0sNub05qE1NcV0Ti8b8b9B2tt5yPwC3XQloe4os=" sh -s -- -y
+curl https://agent.connect.nginx.com/nginx-agent/install | DATA_PLANE_KEY="yIyz0sNub05qE1NcV0Ti8b8b9B2tt5yPwC3XQloe4os=" sh -s -- -y
