dea34628b91a7b53f3e9750f188f242905eefd2f
Stephen Archer <s.archer@f5.com>
2025-10-14 14:07:08 +0100

    updated to add scripts page

--- http.js
+++ http.js
@@ -47,7 +47,7 @@ function generateScripts() {
   Insert this inside <head>.
 */
 (async function showQuotableAlert(){
-  const url = "zenquotes.io/api/quotes/random;
+  const url = "zenquotes.io/api/quotes/random";
   try {
     const res = await fetch(url, { cache: "no-store" });
     if(!res.ok) throw new Error("Network response not OK");
